86ddf1a4331cb49ba099f347f31219ec08414687
aLynn8 <61812849+aLynn8@users.noreply.github.com>
2021-03-16 18:57:38 -0600

    update

---
+++ TempleScheduler/Models/Cart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TempleScheduler.Models
+{
+    public class Cart
+    {
+        public List<CartLine> Lines { get; set } = new List<CartLine>();
+
+        public void AddItem ()
+
+
+    }
+}
