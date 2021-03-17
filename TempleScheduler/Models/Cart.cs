50c6208e9d6e9b9657625df5e56acc0b5f470db1
aLynn8 <61812849+aLynn8@users.noreply.github.com>
2021-03-16 19:17:00 -0600

    added itms from model to Confirmation Page

--- TempleScheduler/Models/Cart.cs
+++ TempleScheduler/Models/Cart.cs
@@ -1,13 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
 
-namespace TempleScheduler.Models
-{
-    public class Cart
-    {
-        
+//namespace TempleScheduler.Models
+//{
+//    public class Cart
+//    {
+//        public List<CartLine> Lines { get; set } = new List<CartLine>();
 
-    }
-}
+//        public void AddItem (Group group)
+
+//    }
+
+//    //this is what will be stored for each line item in the cart
+//    public class CartLine
+//    {
+//        public int CartLineID { get; set; }
+//        public Book Book { get; set; }
+//        public int Quantity { get; set; }
+//    }
+
+
+//}
