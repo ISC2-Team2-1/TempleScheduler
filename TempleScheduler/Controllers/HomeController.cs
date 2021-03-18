122682439d30f6d0a6221afa6b63fb9447219eb7
Tyler Barty <tyler.barty@gmail.com>
2021-03-17 20:19:39 -0600

    homecontroller to have times

--- TempleScheduler/Controllers/HomeController.cs
+++ TempleScheduler/Controllers/HomeController.cs
@@ -37,6 +37,25 @@ namespace TempleScheduler.Controllers
         {
             if (ModelState.IsValid)
             {
+
+                List<string> time_list = new List<string>()
+                {
+                    "8:00am - 9:00am",
+                    "9:00am - 10:00am",
+                    "10:00am - 11:00am",
+                    "11:00am - 12:00pm",
+                    "12:00pm - 1:00pm",
+                    "1:00pm - 2:00pm",
+                    "2:00pm - 3:00pm",
+                    "3:00pm - 4:00pm",
+                    "4:00pm - 5:00pm",
+                    "5:00pm - 6:00pm",
+                    "6:00pm - 7:00pm",
+                    "7:00pm - 8:00pm",
+                    "8:00pm - 9:00pm",
+                };
+
+                ViewBag.TimesList = time_list;
                 ViewBag.Timeslots = timeslot;
                 ViewBag.time = timeslot.Timeslots;
                 return View("Form");
